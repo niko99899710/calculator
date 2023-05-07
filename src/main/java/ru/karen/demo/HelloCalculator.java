4922dc29a17cb54cfcb2aa9aff292146bc43b1d6
Karina <bagdasarova-k@inbox.ru>
2023-05-07 22:13:38 +0500

    Calculator

---
+++ src/main/java/ru/karen/demo/HelloCalculator.java
@@ -0,0 +1,49 @@
+package ru.karen.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloCalculator {
+    private final ServiceCalculator calculateService;
+    private final ServiceCalculator serviceCalculator;
+    private final ServiceCalculator service12;
+    @Autowired
+    public HelloCalculator(ServiceCalculator calculateService, ServiceCalculator serviceCalculator, ServiceCalculator service12) {
+        this.calculateService = calculateService;
+        this.serviceCalculator = serviceCalculator;
+        this.service12 = service12;
+    }
+
+    @GetMapping
+    public String Calculator () {
+        return calculateService.calculator();
+    }
+
+    @GetMapping(path = "/calculator")
+    public String writeCalculator () {
+        return calculateService.writeCalculator();
+    }
+
+    @GetMapping(path = "/plus?num1=5&num2=5")
+    public Integer plus(@RequestParam ("5") Integer num1, @RequestParam ("5") Integer  num2) {
+        return serviceCalculator.plus(5, 5);
+    }
+
+    @GetMapping(path = "/minus?num1=5&num2=5")
+    public Integer minus(@RequestParam ("5") Integer num1, @RequestParam ("5") Integer  num2) {
+        return serviceCalculator.plus(5, 5);
+    }
+
+    @GetMapping(path = "/multiply?num1=5&num2=5")
+    public Integer multiply(@RequestParam ("5") Integer num1, @RequestParam ("5") Integer  num2) {
+        return serviceCalculator.plus(5, 5);
+    }
+
+    @GetMapping(path = "/divide?num1=5&num2=5")
+    public Integer divide(@RequestParam ("5") Integer num1, @RequestParam ("5") Integer  num2) {
+        return serviceCalculator.plus(5, 5);
+    }
+}
---
+++ src/main/java/ru/karen/demo/ServiceCalculator.java
@@ -0,0 +1,15 @@
+package ru.karen.demo;
+
+public interface ServiceCalculator {
+
+    String calculator();
+    String writeCalculator();
+
+    int num2 = 5;
+    int num1 = 5;
+    Integer plus(Integer num1, Integer num2);
+    Integer minus(Integer num1, Integer num2);
+    Integer multiply(Integer num1, Integer num2);
+    Integer divide(Integer num1, Integer num2);
+
+}
